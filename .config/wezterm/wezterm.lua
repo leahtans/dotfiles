900543cda6193b67f0897b48a6e9965dc57493de
Leah Tans <leahtans@proton.me>
2025-02-23 11:52:54 +0000

    fixed wezterm using incorrect cursor theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -16,4 +16,17 @@ config.enable_scroll_bar = true
 
 config.color_scheme = 'Ayu Dark (Gogh)'
 
+-- Code snippet from: https://github.com/wezterm/wezterm/issues/1742#issuecomment-1075333507
+-- Finds currently in use cursor theme and cursor size, applies them to the wezterm config
+local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-theme"})
+if success then
+  config.xcursor_theme = stdout:gsub("'(.+)'\n", "%1")
+end
+
+local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-size"})
+if success then
+  config.xcursor_size = tonumber(stdout)
+end
+-- Code snippet end
+
 return config
